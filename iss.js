6d5fa96956bba568e27519b70fa7e1e05626caa1
Robert Shum <robertshum@gmail.com>
2023-09-27 19:12:50 -0700

    linter fixes.

--- iss.js
+++ iss.js
@@ -3,7 +3,7 @@ const request = require("request");
 /**
  * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback with an error or results. 
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly-over times as an array (null if error):
